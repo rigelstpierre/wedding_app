1b8a34821370546b2cbcc37fc7146771c3239b1f
Rigel St. Pierre <rigel@rigel.co>
2014-03-22 23:38:36 -0700

    intail setup

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'simple_form'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingApp::Application.config.secret_key_base = '07e110906ee2793880b51b32c1ed7e0a734082cc20caee4a09250db254c9a4125f4bcbd50f3ff711568ac6a742cca09eb9ac6cbc1114fc705671f09ad6f5f6d9'
